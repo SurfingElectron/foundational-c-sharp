2260ffebb72c266a5a2bbf30257a6e451b35477f
Emma Gill <e.e.gill@outlook.com>
2023-09-10 20:49:33 +0100

    Working through for loops.

---
+++ 3-add-logic-to-console-apps/4-for-loops/Program.cs
@@ -0,0 +1,52 @@
+﻿// ITERATE THROUGH A CODE BLOCK USING FOR STATEMENT IN C# 
+
+// EXERCISE: CREATE AND CONFIGURE FOR ITERATION LOOPS
+/*
+Console.WriteLine("ASCENDING");
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("DESCENDING");
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("INCREMENT BY 3");
+for (int i = 0; i < 10; i += 3)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("BREAK AT 7");
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+
+Console.WriteLine("LOOP THROUGH AN ARRAY");
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length -1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+Console.WriteLine("CHANGE A VARIABLE WHILE LOOPING");
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) 
+    Console.WriteLine(name);
+*/
+
+// EXERCISE: COMPLETE A CHALLENGE ACTIVITY USING FOR AND IF STATEMENTS
+/*
+Output values from 1 to 100, one number per line, inside the code block of an iteration statement.
+When the current value is divisible by 3, print the term Fizz next to the number.
+When the current value is divisible by 5, print the term Buzz next to the number.
+When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number.
+*/
